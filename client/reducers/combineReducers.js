c40b259505dffa9f90b8719fb3278e284b1e2607
Joseph Nagy <nagyjoseph9@gmail.com>
2021-12-28 20:50:23 -0500

    added applicationReducer to global reducer

--- client/reducers/combineReducers.js
+++ client/reducers/combineReducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import pageReducer from './pageReducer';
+import applicationReducer from './applicationReducer';
 
 export default combineReducers({
   page: pageReducer,
+  application: applicationReducer
 });                                      
